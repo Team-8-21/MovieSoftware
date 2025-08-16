1ab3ad3e6428650609003ec7134c36f1ca607816
Melinda Allen <msal71396@edu.ucl.dk>
2025-08-16 14:57:52 +0200

    Login_FirstTake

---
+++ MVVM/ViewModel/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieSoftware.MVVM.Model.Classes;
+using MovieSoftware.MVVM.Model.Repositories;
+using MovieSoftware.Parent_classes;
+
+namespace MovieSoftware.MVVM.ViewModel
+{
+    public class UserViewModel : ViewModelBasic
+    {
+        //Arbejder vi med CSV, så skal det ændres fx clients.csv
+        private readonly IUserRepository userRepository = new FileUserRepository("clients.txt");
+
+        public ObservableCollection<User> Users { get; }
+        public ICollectionView UsersCollectionView {get;}
+
+        private string name;
+        public string Name { get => name; set { name = value; OnPropertyChanged(); } }
+
+    }
+}
